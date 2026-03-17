ad9a0ca24fb5227984ccba87f1b22519ba775a57
Graham Mix <graham.mix@hotmail.com>
2026-03-17 12:15:30 -0400

    Add analytics to layout

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import "../globals.css";
 import Header from "@/components/Header"
 import Footer from "@/components/Footer";
@@ -43,6 +44,7 @@ export default function RootLayout({
               </main>
             <Footer />
           </div>
+          <Analytics />
     </ClerkProvider>
   );
 }
