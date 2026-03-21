0f353ce8b31669633edae02aeec6c26e263f84df
Graham Mix <graham.mix@hotmail.com>
2026-03-21 09:13:07 -0400

    Add debug statement to verify environment variables

--- src/components/HomeBanner.tsx
+++ src/components/HomeBanner.tsx
@@ -2,6 +2,7 @@ import { Title } from './ui/text';
 import React from 'react';
 
 const HomeBanner = () => {
+  console.log("Secret key prefix:", process.env.CLERK_SECRET_KEY?.substring(0, 8));
   return (
       <div className="bg-[url('/images/playmats/Containment_Breach_Playmat_Watermarked.png')] bg-cover bg-position-[center_6.5%] mt-4 mb-4 py-16 md:py10 rounded-lg lg:px-17 max-h-35 flex items-center justify-between">
         <div>
